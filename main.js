d7bb4f4ac5ebd713c4e2273fae69759868f8b2e8
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-11-06 21:19:00 -0800

    simple todo list complete

--- main.js
+++ main.js
@@ -2,31 +2,52 @@ let addToButton = document.getElementById("addTo");
 let toDoContainer = document.getElementById("todoContainer");
 let inputField = document.getElementById("input");
 
-function addInput() {
+function createInput() {
+  let toDoTitle = document.createElement("h3");
   let inputContainer = document.createElement("div");
   let newInput = document.createElement("input");
   let inputButton = document.createElement("button");
   newInput.classList.add("newInput-styles");
+  inputButton.classList.add("newBtn-styles");
 
-  toDoContainer.appendChild(inputContainer);
+  inputButton.addEventListener("click", () => {
+    const newItem = addItem(newInput.value);
+    inputContainer.appendChild(newItem);
+  });
+
+  inputButton.innerText = "+";
+
+  toDoTitle.innerText = inputField.value;
+
+  inputContainer.appendChild(toDoTitle);
   inputContainer.appendChild(newInput);
   inputContainer.appendChild(inputButton);
+  return inputContainer;
 }
 
-function addItem() {
-  let listItem = document.createElement("h3");
-  listItem.classList.add("listItem-styling");
-  listItem.innerText = inputField.value;
-  toDoContainer.appendChild(listItem);
-  inputField.value = "";
+const addInput = () => {
+  const inputContainer = createInput();
+  toDoContainer.appendChild(inputContainer);
+};
 
-  listItem.addEventListener("click", function () {
-    listItem.style.textDecoration = "line-through";
+function addItem(text) {
+  let newListItem = document.createElement("p");
+
+  newListItem.innerText = text;
+  newListItem.classList.add("listItem-styling");
+
+  newListItem.addEventListener("click", function () {
+    newListItem.style.textDecoration = "line-through";
   });
 
-  listItem.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(listItem);
+  newListItem.addEventListener("dblclick", function () {
+    newListItem.remove();
   });
+  return newListItem;
+  // let listItem = document.createElement("h3");
+  // listItem.innerText = inputField.value;
+  // toDoContainer.appendChild(listItem);
+  // inputField.value = "";
 }
 
 addToButton.addEventListener("click", addInput);
